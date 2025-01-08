2fe1120c72cbd11848de669e843ac935afff5d1a
hungNv135 <nguyenhungvan135@gmail.com>
2025-01-08 18:45:02 +0700

    Lab1

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Nguyen Van Hung - PH48907</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
